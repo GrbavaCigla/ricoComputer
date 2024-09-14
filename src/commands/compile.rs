afce9dc7b97095e388bc33fd4b0194ab3cbaa7e0
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-09-14 16:24:30 +0200

    Added nom_supreme

--- src/commands/compile.rs
+++ src/commands/compile.rs
@@ -1,6 +1,7 @@
 use std::{fs::read_to_string, path::Path};
 
 use nom::Finish;
+use nom_supreme::error::ErrorTree;
 
 use crate::parsers::parse;
 
@@ -9,7 +10,13 @@ pub fn run<P: AsRef<Path>>(source: P, _output: Option<P>) {
     // TODO: Remove unwrap
     let source = read_to_string(source).unwrap();
     // let syntax: crate::types::SyntaxTree = final_parser(parse)(&source).unwrap();
-    let syntax: crate::types::SyntaxTree = parse::<()>(&source).finish().unwrap().1;
+    let syntax = match parse::<ErrorTree<&str>>(&source).finish() {
+        Ok((_, tree)) => tree,
+        Err(e) => {
+            eprintln!("{:#?}", e);
+            return;
+        },
+    };
 
     println!("{:#?}", syntax);
 
