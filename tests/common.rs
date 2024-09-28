5ea4d443230c31ff8f6b229ec2dd1908c91b61d7
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-09-28 15:19:23 +0200

    Split test into parser and assembler tests.

--- tests/common.rs
+++ tests/common.rs
@@ -2,19 +2,17 @@ use std::{ffi::OsStr, fs::read_to_string, path::PathBuf};
 
 use walkdir::WalkDir;
 
-pub fn get_example_sources() {
+pub fn get_example_sources() -> impl Iterator<Item = (PathBuf, String)> {
     let mut directory = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     directory.push("examples");
 
-    for entry in WalkDir::new(directory)
+    WalkDir::new(directory)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
         .filter(|e| match e.path().extension().and_then(OsStr::to_str) {
             Some(p) => p == "pca",
             None => false,
-        }).map(|e| read_to_string(e.path()))
-    {
-
-    }
+        })
+        .map(|e| (PathBuf::from(e.path()), read_to_string(e.path()).unwrap()))
 }
